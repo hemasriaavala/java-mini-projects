1924e0132719ec84e4be5acdff6965801f14a5a4
Aavala Hema Sri <hemasriaavala@gmail.com>
2025-10-27 22:52:40 +0530

    Create SingleDigitSum.java

---
+++ SingleDigitSum.java
@@ -0,0 +1,21 @@
+// SingleDigitSum.java
+import java.util.Scanner;
+
+class SingleDigitSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        while (n >= 10) {
+            int sum = 0;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }
+
+        System.out.println("Final single digit: " + n);
+    }
+}
